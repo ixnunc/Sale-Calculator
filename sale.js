576ff49a58faa63cfe489655c0851bd023569b74
ixnunc <49885706+ixnunc@users.noreply.github.com>
2020-11-09 11:46:01 -0500

    Update sale.js
    Removes variables as they are not needed and updates application to take user input and clear fields.

--- sale.js
+++ sale.js
@@ -1,9 +1,3 @@
-	// setting variables to workable values
-let base = 0;
-let total = 0;
-let sale = (100 - (document.getElementById("sale_input").value))/100;
-
-
 	// functions to calculate total
 function calc15() {
 	document.getElementById("total").value = document.getElementById("base").value * .85;
@@ -20,12 +14,14 @@ function calc45() {
 function calc65() {
 	document.getElementById("total").value = document.getElementById("base").value * .35;
 }
-
+	// function converts user input to sale value
 function calcsale() {
-	document.getElementById("total").value = document.getElementById("base").value * sale;
+	document.getElementById("total").value = document.getElementById("base").value * (100-(document.getElementById("sale_input").value))/100;
 }
 
 	// resets variables so user can enter different values; assigned to clear button
-function clear() {
-	
+function clears() {
+	document.getElementById("total").value = "";
+	document.getElementById("base").value = "";
+	document.getElementById("sale_input").value = "";
 }
